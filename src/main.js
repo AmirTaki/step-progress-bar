575e2bba2a3d3358cf3e6664dc27d6d66f19d513
amir <amirtaki623@gmail.com>
2025-08-02 23:10:16 +0330

    js part 4

--- src/main.js
+++ src/main.js
@@ -2,3 +2,10 @@ const nextEl =  document.getElementById("next")
 const prevEl = document.getElementById("prev")
 const progressEl = document.querySelector(".progress-bar-front")
 const stepsEl = document.querySelectorAll('.step')
+
+let currentChecked = 1;
+
+nextEl.addEventListener("click", ()=> {
+    currentChecked++;
+    
+})
