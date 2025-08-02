6f0b91920410497571f58dfe67eeea4544c8e1cd
amir <amirtaki623@gmail.com>
2025-08-02 23:09:41 +0330

    js part 2

--- src/main.js
+++ src/main.js
@@ -1,2 +1,4 @@
 const nextEl =  document.getElementById("next")
 const prevEl = document.getElementById("prev")
+const progressEl = document.querySelector(".progress-bar-front")
+const stepsEl = document.querySelectorAll('.step')
